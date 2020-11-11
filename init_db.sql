603cb16bbe5332306ad5c7e3cf08c435479b56b0
Stas <kurushinstas@gmail.com>
2020-11-11 22:28:52 +0300

    feature: added postProduct lambda func (#1)
    Co-authored-by: Stanislav Kurushin <Stanislav_Kurushin@epam.com>

--- init_db.sql
+++ init_db.sql
@@ -1,6 +1,7 @@
 INSERT INTO products (title, description, price) VALUES ('Mocked product from db', 'Short Product Description1', 10);
 INSERT INTO products (title, description, price) VALUES ('Mocked product from db', 'Short Product Description2', 9);
 INSERT INTO products (title, description, price) VALUES ('Mocked product from db', 'Short Product Description3', 8);
+INSERT INTO products (title, description, price) VALUES ('Mocked product from db', 'Short Product Description3', 7);
 
 INSERT INTO stocks (count, product_id) VALUES (2, '5dd4d7ca-9e8e-49f6-9cff-6c9873232206');
 INSERT INTO stocks (count, product_id) VALUES (2, '19d0c646-107a-4aa7-8771-9b7c6f017f6c');
